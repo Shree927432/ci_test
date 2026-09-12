9957ee94340e34e859d28af9c14854ab8243969f
Shrey <spawade54@gmail.com>
2026-09-12 19:34:39 +0530

    Remove flaky timing test from test_app.py
    Removed flaky timing test that fails intermittently.

--- test_app.py
+++ test_app.py
@@ -1,10 +1,4 @@
 from app import add
-import time
-
-def test_flaky_timing():
-    # Fails roughly 50% of the time based on epoch second
-    assert int(time.time()) % 2 == 0, "Transient network timing blip"
-
 
 
 def test_add():
