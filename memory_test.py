2ef7490d4e82e9d196c41236c6f4a1b56685518a
Shrey <spawade54@gmail.com>
2026-09-12 19:56:45 +0530

    Disable memory limit test in memory_test.py
    Comment out memory limit test and related code.

--- memory_test.py
+++ memory_test.py
@@ -1,11 +1,11 @@
 # memory_test.py
-import resource
+# import resource
 
-def test_runner_memory():
-    # Set hard address space limit to 256 MB
-    limit = 256 * 1024 * 1024
-    resource.setrlimit(resource.RLIMIT_AS, (limit, limit))
+# def test_runner_memory():
+#     # Set hard address space limit to 256 MB
+#     limit = 256 * 1024 * 1024
+#     resource.setrlimit(resource.RLIMIT_AS, (limit, limit))
     
-    chunks = []
-    while True:
-        chunks.append(b"x" * (50 * 1024 * 1024))
+#     chunks = []
+#     while True:
+#         chunks.append(b"x" * (50 * 1024 * 1024))
